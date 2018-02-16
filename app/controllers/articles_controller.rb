c715d6ef83fe84e22b73d2d67d267dca72b7823d
Ivan2k <motoface@yandex.ru>
2018-02-16 19:53:03 +0300

    to articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,9 @@
 class ArticlesController < ApplicationController
 	
-	
+	def index
+		@show_articles = Article.all
+	end
+
 	def new
 		
 	end
@@ -10,15 +13,14 @@ class ArticlesController < ApplicationController
 
 		if @art.valid?
 			@art.save
+			# что бы небыло двойного заноса данных автоматом перенаправляет на занесённую статью
+			redirect_to @article
 		else
 			render action: 'new'
 		end
 	end
 
-	def show
-		@show_articles = Article.all
-	end
-
+	
 	def destroy
 		
 	end
