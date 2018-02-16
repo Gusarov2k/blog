c6f54313654fe4b144ecbf8f5b10a49033d47c72
Ivan2k <motoface@yandex.ru>
2018-02-16 11:15:21 +0300

    Save contacts to db

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,6 +5,16 @@ class ContactsController < ApplicationController
 	end
 
 	def create
+		# в качестве аргумента передаётся функция 
+		@contact = Contact.new(contact_params)
+		@contact.save
 	end
+# create methods closed для разрешения доступа к атрибутам
+	private
 
+	def contact_params
+		# разрешение доступа к хешу параметров формы и перечисление нужных полей
+		params.require(:contact).permit(:email, :message)
+		
+	end
 end
